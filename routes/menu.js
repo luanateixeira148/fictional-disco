7d1420080370e535256b62546ba5c3fc5aabef64
Luana <luanateixeira148@gmail.com>
2021-07-27 01:17:51 +0000

    change menu ejs to accept database input

--- routes/menu.js
+++ routes/menu.js
@@ -2,9 +2,32 @@ const express = require('express');
 const router = express.Router();
 
 
+const tempMenuDatabase = [
+  {
+    id: 1,
+    name: 'Rapberry Ice Cream',
+    description: 'Delicious raspberry ice cream',
+    price: '$11',
+    photo_url: 'https://d2cbg94ubxgsnp.cloudfront.net/Pictures/1024x536/4/0/8/132408_shutterstock_406445776.jpg',
+    category: 'fruits'
+  },
+  {
+    id: 2,
+    name: 'Lemon Ice Cream',
+    description: 'Amazing lemon ice cream',
+    price: '$9',
+    photo_url: 'https://www.seasonsandsuppers.ca/wp-content/uploads/2016/02/lemon-icecream1200.jpg',
+    category: 'fruits'
+  },
+]
+
 // GET /menu
 router.get ('/', (req, res) => {
-  res.render('menu');
+
+  const templateVars = { menuItems: tempMenuDatabase };
+
+  res.render('menu', templateVars);
+
 });
 
 module.exports = router;
