b133833c0d19cda04e50c874054173c9a311073d
Raphaella Robles <raphaella.robles@gmail.com>
2021-07-27 05:56:37 +0000

    Update images and values of items table within seeds

--- db/schema/01_schema.sql
+++ db/schema/01_schema.sql
@@ -4,18 +4,20 @@
 -- CREATE DATABASE midterm OWNER labber;
 
 -- Helps re run the files easily by clearing any existing tables first
-DROP TABLE IF EXISTS customers CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS categories CASCADE;
 DROP TABLE IF EXISTS items CASCADE;
 DROP TABLE IF EXISTS orders CASCADE;
 DROP TABLE IF EXISTS order_items CASCADE;
 
--- Run file against midterm database using: \i migrations/01_schema.sql
-CREATE TABLE customers (
+-- Run file against database using: \i migrations/01_schema.sql
+CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
-  phone_number VARCHAR(10) NOT NULL,
-  email VARCHAR(255)
+  phone_number VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  is_admin BOOLEAN NOT NULL DEFAULT FALSE
 );
 
 CREATE TABLE categories (
@@ -27,21 +29,23 @@ CREATE TABLE items (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
   description TEXT,
-  price DECIMAL(4,2) NOT NULL,
+  price INTEGER NOT NULL DEFAULT 0, -- in cents
   photo_url VARCHAR(255) NOT NULL,
   category_id INTEGER REFERENCES categories(id) ON DELETE CASCADE
 );
 
 CREATE TABLE orders (
   id SERIAL PRIMARY KEY NOT NULL,
-  customer_id INTEGER REFERENCES customers(id) ON DELETE CASCADE,
-  created_at TIMESTAMP NOT NULL
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  created_at TIMESTAMP,
+  completed_at TIMESTAMP,
+  order_status VARCHAR(255) DEFAULT 'SUBMITTED',
+  pickup_time INTEGER -- in minutes
 );
--- total price can be SQL query that gets calculated at the end
 
 CREATE TABLE order_items (
   id SERIAL PRIMARY KEY NOT NULL,
+  item_id INTEGER REFERENCES items(id) ON DELETE CASCADE,
   order_id INTEGER REFERENCES orders(id) ON DELETE CASCADE,
-  item_id INTEGER REFERENCES orders(id) ON DELETE CASCADE,
   quantity INTEGER NOT NULL DEFAULT 1
 );
--- db/seeds/01_seeds.sql
+++ db/seeds/01_seeds.sql
@@ -1,43 +1,53 @@
--- Add values into customers table
-INSERT INTO customers (name, phone_number, email)
-VALUES ('Raphaella Robles', '7789869424', 'raphaella.robles@gmail.com');
+-- Add values into users table for customers
+INSERT INTO users (name, phone_number, email, password)
+VALUES ('Raphaella Robles', '7789869424', 'raphaella.robles@gmail.com', 'customer');
+-- Add values into users table for admin
+INSERT INTO users (name, phone_number, email, password, is_admin)
+VALUES ('Admin', '1234567890', 'admin@gmail.com', 'admin', TRUE);
 
 -- Add values into categories table
-INSERT INTO categories (name) VALUES ('Milky'), ('Fruity');
+INSERT INTO categories (name) VALUES ('Milky'), ('Fruity'), ('Smoothie');
 
 -- Add values into items table
 INSERT INTO items (name, description, price, photo_url, category_id)
 VALUES
-('Brown Sugar Milk Tea',
-'Signature milk tea with brown sugar syrup topped with pearls',
-5.50, 'https://chatime.com/wp-content/uploads/2020/10/Brown-Sugar-Pearls-with-Fresh-Milk-250x314.png',
-1),
+('Brown Sugar Milk Tea', 'Signature milk tea with brown sugar syrup', 550,
+'https://www.ohhowcivilized.com/wp-content/uploads/brown-sugar-tea-latte-7.jpg', 1),
 
-('Roasted Milk Tea',
-'Delicious Japanese milk tea topped with grass jelly',
-5.50,
-'https://chatime.com/wp-content/uploads/2019/04/Grass-Jelly_Roasted-Milk-Tea-250x314.png',
-1),
+('Roasted Milk Tea', 'Delicious Japanese milk tea topped with pearls',
+550, 'https://www.ohhowcivilized.com/wp-content/uploads/bubble-tea-5.jpg', 1),
 
-('Taro Milk Tea',
-'Robust milk tea made with real ground taro',
-5.50,
-'https://chatime.com/wp-content/uploads/2019/04/Taro-Milk_Tea-250x314.png',
-1),
+('Thai Milk Tea', 'Robust black tea sweetened with evaporated milk', 550,
+'https://www.ohhowcivilized.com/wp-content/uploads/thai-iced-tea-7.jpg', 1),
 
-('Matcha Milk Tea', 'Earthy matcha mixed with fresh milk', 5.00, 'https://chatime.com/wp-content/uploads/2019/04/Matcha-Tea-Latte-250x314.png', 1),
+('Matcha Milk Tea', 'Earthy matcha mixed with fresh milk', 500,
+'https://www.ohhowcivilized.com/wp-content/uploads/iced-matcha-latte-10.jpg', 1),
 
-('Earl Grey Milk Tea', 'Bold and rich milk tea with a hint of bergamot', 5.00, 'https://chatime.com/wp-content/uploads/2019/04/Earl-Grey-Milk-Tea-250x314.png', 1),
+('Earl Grey Milk Tea', 'Bold and rich milk tea with a hint of bergamot', 500,
+'https://www.ohhowcivilized.com/wp-content/uploads/starbucks-iced-chai-tea-latte-8.jpg', 1),
 
-('Passion Fruit Tea', 'Sweet passion fruit green tea with passion fruit seeds', 5.00, 'https://chatime.com/wp-content/uploads/2019/04/Passionfruit-Green-Tea-250x314.png', 2),
+('Passionfruit Iced Tea', 'Sweet and refreshing passionfruit iced tea', 500, 'https://www.ohhowcivilized.com/wp-content/uploads/starbucks-iced-passion-tango-tea-7a.jpg', 2),
 
-('Grapefruit Tea', 'Jasmine green tea with bold and tangy citrus flavours', 5.00,'https://chatime.com/wp-content/uploads/2019/04/Grapefruit-Green-Tea-250x314.png', 2),
+('Hibiscus Iced Tea', 'Jasmine green tea with bold and tangy citrus flavours', 500,
+'https://www.ohhowcivilized.com/wp-content/uploads/1019-sparkling-hibiscus-yerba-mate-8.jpg', 2),
 
-('Strawberry Tea', 'A floral green tea with a strawberry flavour', 5.00, 'https://chatime.com/wp-content/uploads/2020/10/Strawberry-Greeen-tea-250x314.png', 2),
+('Berry Iced Tea', 'A floral green tea with a strawberry flavour', 500, 'https://www.ohhowcivilized.com/wp-content/uploads/2019/06/0619-raspberry-iced-tea-8.jpg', 2),
 
-('Mango Tea', 'A swirl of tropical mango makes a tangy, sweet tea', 5.00, 'https://chatime.com/wp-content/uploads/2020/10/Mango-Grapefruit-250x314.png', 2),
+('Peach Iced Tea', 'A peach-filled tangy, sweet iced tea', 500,
+'https://www.ohhowcivilized.com/wp-content/uploads/peach-iced-tea-10.jpg', 2),
 
-('Winter Melon Tea', 'A sweet iced tea made with real winter melon', 5.00, 'https://chatime.com/wp-content/uploads/2020/10/Candied-Winter-Melon-Ice-Tea-250x314.png', 2);
+('Lemon Iced Tea', 'A sweet iced tea made with fresh lemons', 500,
+'https://www.ohhowcivilized.com/wp-content/uploads/2019/05/0519-lemon-iced-tea-10.jpg', 2),
+
+('Rose Smoothie', 'A delightful mix of strawberries, rose petals, and hibiscus', 600, 'https://www.ohhowcivilized.com/wp-content/uploads/2019/07/0719-frozen-strawberry-rose-green-tea-10.jpg', 3),
+
+('Hojicha Smoothie', 'Frozen hojicha tea latte with blended with dulce de leche', 600, 'https://www.ohhowcivilized.com/wp-content/uploads/2019/07/0719-hojicha-tea-frappuccino-8.jpg', 3),
+
+('Thai Tea Smoothie', 'Sweet, creamy blended frozen Thai iced tea', 600, 'https://www.ohhowcivilized.com/wp-content/uploads/2019/07/0719-thai-tea-frappuccino-11-1.jpg', 3),
+
+('Matcha Smoothie', 'Sweetened and blended frozen matcha green tea latte', 600, 'https://www.ohhowcivilized.com/wp-content/uploads/2019/07/0719-matcha-frappuccino-9-1.jpg', 3),
+
+('London Fog Smoothie', 'Frozen earl grey tea latte blended and topped with whipped cream', 600, 'https://www.ohhowcivilized.com/wp-content/uploads/london-fog-frappuccino-010.jpg', 3);
 
 -- Add values into orders table
 
