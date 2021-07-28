0681747d77143fc8649edf1425e303de4c04ec41
BeamMeup-fail <51726921+BeamMeup-fail@users.noreply.github.com>
2021-07-28 13:38:35 -0700

    Create twilio.js

---
+++ API/twilio.js
@@ -0,0 +1,16 @@
+const accountSid = process.env.ACCOUNT_TWILIO ;
+const authToken = process.env.TOKEN_TWILIO;
+const client  = require('twilio')(accountSid, authToken);
+
+
+const message = `Hello ${(customer)}, your order from place idk the name of should be ready in ${time} mins! You can check the ETA at ${order_url}.`
+
+client.messages.create({
+    body: message,
+    to: '+12345678901', // Text this number
+    from: '+18722334590', // From a valid Twilio number
+}, (err, message) => {
+  if (err) {
+    return null;
+  }
+});
