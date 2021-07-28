8fb294e76ab952b0e9a45d59ef8c1b9e77e7cdb7
BeamMeup-fail <51726921+BeamMeup-fail@users.noreply.github.com>
2021-07-28 16:08:59 -0700

    Update twilio.js

--- API/twilio.js
+++ API/twilio.js
@@ -1,35 +1,37 @@
-const accountSid = process.env.ACCOUNT_TWILIO ;
+const accountSid = process.env.ACCOUNT_TWILIO;
 const authToken = process.env.TOKEN_TWILIO;
 const client  = require('twilio')(accountSid, authToken);
 
 
-const messageCustomer = (customer, restaurant, time, order_url) => { //sends a message about when the order is ready and if they want to check the eta
-  const message = `Hello ${(customer)}, your order from ${restaurant} should be ready in ${time} mins! You can check the ETA at ${order_url}.`
-
-client.messages.create({
-    body: message,
-    to: '+12345678901', // Text this number
-    from: '+18722334590', // From a valid Twilio number
-}, (err, message) => {
-  if (err) {
-    return null;
-  }
-});
-}
-
-const messComplete = (customer, restaurant, order_url) => {//sends a message about when the order is ready and if they want to check the eta
-  const message = `Hello ${(customer)}, your order from ${restaurant} is now ready! You can verify at ${order_url}.`
-
+const messageCustomer = (customer, number, time) => { //sends a message about when the order is ready and if they want to check the eta
+  const message = `Hello ${(customer)}, your order from Teashop should be ready in ${time} mins!`;
+  number = String(number);
   client.messages.create({
     body: message,
-    to: '+12345678901', // Text this number
+    to: number, // Text this number
+    from: '+18722334590', // From a valid Twilio number
+  },(err, message) => {
+    if (err) {
+      return null;
+    }
+    return message;
+  });
+};
+
+const messComplete = (customer, number) => {//sends a message about when the order is ready and if they want to check the eta
+  const message = `Hello ${(customer)}, your order from Teashop is now ready!`;
+  number = String(number);
+  client.messages.create({
+    body: message,
+    to: number, // Text this number
     from: '+18722334590',
   }, (err, message) => {
     if (err) {
       return null;
     }
+    return message;
   });
-}
+};
 
 
 
