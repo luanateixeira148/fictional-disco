0670acc5f1807008a90dae2558aeab1c9344b57e
Raphaella Robles <raphaella.robles@gmail.com>
2021-07-28 05:58:44 +0000

    Test current get routes to menu and orders

--- routes/items.js
+++ routes/items.js
@@ -1,23 +1,31 @@
 const express = require('express');
 const router = express.Router();
 
-module.exports = function (db) {
+module.exports = function(db) {
 
   // GET /menu
-  /* Render the menu page */
   router.get('/', (req, res) => {
-    console.log("connected to database!");
+    console.log("getting menu items from database...");
     const sql = `SELECT * FROM items`;
     db.query(sql)
       .then((data) => {
         console.log("data.rows: ", data.rows);
         const templateVars = { menuItems: data.rows };
+        /* Render the menu page with all menu items*/
         res.render('menu', templateVars);
       })
 
-
-
-
   });
+
+  // // GET /menu/:id
+  // router.get('/:id', (req, res) => {
+  //   db.query('SELECT * FROM items WHERE id = $1;', [req.params.id])
+  //     .then((data) => {
+  //       const templateVars = { menuItems: data.rows[0] };
+  //       /* Render the menu page with a single menu item*/
+  //       res.render('menu', templateVars);
+  //     })
+  // });
+
   return router;
 };
--- routes/order_details.js
+++ routes/order_details.js
@@ -1,20 +1,32 @@
 const express = require('express');
 const router = express.Router();
 
-module.exports = function (db) {
+module.exports = function(db) {
+  // POST /order_details
+  // router.post('/', (req, res) => {
+  //   console.log("posting order_details to database");
+  //   const sql = `INSERT INTO order_items ($1, $2);`
+  //   const values = [req.params.item_id, req.params.quantity];
+  //   db.query(sql, values)
+  //     .then((data) => {
+  //       console.log("data.rows: ", data.rows);
+  //       const templateVars = { menuItems: data.rows };
+  //       /* Render the menu page with all menu items*/
+  //       res.render('order_details', templateVars);
+  //     })
+  // });
 
   // GET /order_details
   router.get('/', (req, res) => {
-    console.log("connected to database!");
+    console.log("getting order_items from database!");
     const sql = `SELECT * FROM order_items`;
     db.query(sql)
     .then((data) => {
       console.log("data.rows: ", data.rows);
       const templateVars = { orderItems: data.rows };
-      /* Render the menu page */
+      /* Render the order_details page */
       res.render('order_details', templateVars);
       })
-
   });
   return router;
 };
