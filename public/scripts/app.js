5f2528875605356ec117f5564c91ddc618bd71af
Luana <luanateixeira148@gmail.com>
2021-07-28 21:16:55 +0000

    notes from assistance with andy

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,60 @@
+//************************************************************/
+//Default code
+// $(() => {
+//   $.ajax({
+//     method: "GET",
+//     url: "/api/users"
+//   }).done((users) => {
+//     for(user of users) {
+//       $("<div>").text(user.name).appendTo($("body"));
+//     }
+//   });;
+// });
+//***********************************************************/
+console.log('***App.js is connected***');
+
 $(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
+  // Ajax POST to a route that just adds the item in your cart table
+
+  // volatile memory
+  const cart = [];
+
+  const $form = $('.add-to-order')
+  $form.submit( function (event) {
+    event.preventDefault();
+    const data = $(this).serialize();
+    console.log('data:', data);
+    cart.push(data);
+    console.log(cart);
+    renderCart(cart);
+
+    // if quantity = 0, show error
+
+    // $.post('/menu', data)
+    //   .then(() => {
+
+    //     console.log('YAY');
+    //   })
+
+  });
+
+  const $cart = $('#cart');
+  const renderItem = function(item) {
+    const $item = $(`<h2> ${item} </h2>`);
+    return $item;
+  }
+
+  const renderCart = function(cart) {
+    $cart.empty();
+    for (let item of cart) {
+      const $item = renderItem(item);
+      $cart.append($item);
     }
-  });;
+
+  }
+
+  renderCart(cart);
+
 });
+
+
--- routes/items.js
+++ routes/items.js
@@ -3,17 +3,46 @@ const router = express.Router();
 
 module.exports = function (db) {
 
-  // GET /menu
-  /* Render the menu page */
+  // GET /items
   router.get('/', (req, res) => {
     console.log("connected to database!");
     const sql = `SELECT * FROM items`;
     db.query(sql)
       .then((data) => {
-        console.log("data.rows: ", data.rows);
+        // console.log("data.rows: ", data.rows);
         const templateVars = { menuItems: data.rows };
         res.render('menu', templateVars);
       })
   });
+
+  router.post('/', (req, res) => {
+    console.log('POSTING IS WORKING');
+
+    // insert into the orders table (user id) + RETURNING id (creates the order table)
+
+    // loop through cart array and insert each item into the order table
+    // promise.all
+    const promises = [];
+    for (const item of cart) {
+      //create the promise
+      const promise = db.query(item);
+      promises.push(promise);
+
+    }
+
+    Promise.all(promises).then((response) => {
+
+      //all done here (send SMS here)
+    })
+
+
+
+    res.status(201).send();
+
+  });
+
   return router;
 };
+
+
+
